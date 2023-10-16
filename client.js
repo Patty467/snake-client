d098d548ec1a9eb5d8c9b3bdfe292c6396215010
Patrick Aucoin <aucoinp640@gmail.com>
2023-10-16 13:16:55 -0400

    write out constants

--- client.js
+++ client.js
@@ -1,19 +1,20 @@
 const net = require("net");
+const { IP, PORT } = require("./constants");
 
 // establishes a connection with the game server
 const connect = function () {
   const conn = net.createConnection({
-    host: 'localhost',
-    port: '50541'
+    host: IP,
+    port: PORT
   });
 
   // interpret incoming data as text
   conn.setEncoding("utf8");
   
+  //returns connection alert once connected, and establishes player name
   conn.on("connect", () => {
     console.log("Connection Established");
     conn.write("Name: PJA");
-    // conn.write("Move: up");
   });
 
   conn.on("data", (stream) => {
--- play.js
+++ play.js
@@ -1,6 +1,7 @@
 const { connect } = require('./client.js');
 const { setupInput } = require('./input.js');
 
+// informs player that client is attempting to connect, then establishes a connection with the game server
 console.log("Connecting ...");
 let conn = connect();
 
